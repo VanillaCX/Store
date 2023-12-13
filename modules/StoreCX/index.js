65992a366c4bce371b0850dfaf1f8b41244e9aae
Lee Bowyer <vanillacx@outlook.com>
2023-12-13 11:29:37 +0100

    Added save to database method and session ID regen

--- modules/StoreCX/index.js
+++ modules/StoreCX/index.js
@@ -4,6 +4,20 @@ const expressSession = require('express-session');
 const MongoDBStore = require('connect-mongodb-session')(expressSession);
 
 class StoreCX {
+    constructor(req, store){
+        this.req = req;
+        this.session = this.req.session[store];
+    }
+
+    async set(key, value) {
+        this.session[key] = value;
+        await StoreCX.save(this.req);
+    }
+
+    async get(key) {
+        return this.session[key];
+    }
+
     static store = new MongoDBStore({
         uri: process.env.SESSIONCX_CONNECTION_STRING, 
         databaseName: process.env.SESSIONCX_DATABASE,
@@ -21,14 +35,87 @@ class StoreCX {
             maxAge: 1000 * 60 * 60 * 24 * 7 // 1 week
         },
         store: StoreCX.store,
-        // Boilerplate options, see:
-        // * https://www.npmjs.com/package/express-session#resave
-        // * https://www.npmjs.com/package/express-session#saveuninitialized
-        resave: true,
-        saveUninitialized: true
+        saveUninitialized: false,
+        resave: false,
     })
+
+    static async save(req){
+        const promise = new Promise((resolve, reject) => {
+            if(!req.session){
+                reject(new Error("NO_SESSION"))
+            } else {
+                req.session.save((error) => {
+                    if (error) {
+                        reject(error)
+                    } else {
+                        resolve();
+                    }
+                })
+            }
+
+        })
+
+        return promise;
+    }
+
+    static async regenerateSessionID(req){
+        const promise = new Promise((resolve, reject) => {
+            if(!req.session){
+                reject(new Error("NO_SESSION"))
+            } else {
+                req.session.regenerate((error) => {
+                    if (error) {
+                        reject(error)
+                    } else {
+                        resolve();
+                    }
+                })
+            }
+        })
+
+        return promise;
+    }
+
+    /**
+     * Example how to set a session var:
+     *      try {
+     *          const userData = new StoreCX(req, "userData");
+     *          await userData.set("name", "Lee");
+     *      } catch(error) {
+     *          // Handle Error
+     *      }
+     * 
+     * 
+     * Example how to get a session var:
+     *      try {
+     *          const userData = new StoreCX(req, "userData");
+     *          const name = userData.get("name");
+     *      } catch(error) {
+     *          // Handle Error
+     *      }
+     * 
+     * 
+     * Example how to regenerate new session ID:
+     *      try {
+     *          await StoreCX.regenerateSessionID(req);
+     *      } catch(error) {
+     *          // Handle Error
+     *      }
+     * 
+     * 
+     * Example how to save session without initialising new StoreCX class instance:
+     *      try {
+     *          await StoreCX.save(req);
+     *      } catch(error) {
+     *          // Handle Error
+     *      }
+     */
     
     
+}
+
+class Session {
+    constructor(){}
 }
 
 module.exports = { StoreCX }
